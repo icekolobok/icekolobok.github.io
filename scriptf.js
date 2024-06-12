59dcd05da825d29959193ef63ea6cd1bbf10b793
icekolobok <svivano@emory.edu>
2024-06-12 00:41:35 -0400

    Initial commit

---
+++ scriptf.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('freq.json')
+        .then(response => response.json())
+        .then(data => {
+            const ctx = document.getElementById('myChart').getContext('2d');
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: data.labels,
+                    datasets: data.datasets
+                },
+                options: {
+                    indexAxis: 'y',
+                    scales: {
+                        x: {
+                            beginAtZero: true,
+                            stacked: false,
+                            position: 'top',
+                            max: 340,
+                            ticks: {
+                                stepSize: 10
+                              }
+                        },
+                        y: {
+                            stacked: true
+                        }
+                    },
+                    responsive: true,
+                    maintainAspectRatio: false
+                
+                }
+            });
+        })
+        .catch(error => console.error('Error loading the JSON data:', error));
+});
